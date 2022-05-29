75aeebe13b09ca3f5290c076f3eb3f5ece570a16
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-05-28 23:07:47 -0400

    corrected syntax errors

--- helpers/uuid.js
+++ helpers/uuid.js
@@ -1,5 +1,5 @@
 // Exports a function that generates a string of random letters and numbers
-module.export = () => 
-    Math.floor((1 = Math.random()) * 0x10000)
+module.exports = () => 
+    Math.floor((1 + Math.random()) * 0x10000)
         .toString(16)
         .substring(1);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const notes = require("./db/db.json");
 const uuid = require("./helpers/uuid.js");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 const app = express();
 
